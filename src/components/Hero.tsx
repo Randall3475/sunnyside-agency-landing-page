4420a2fec0ff5a4e3aee95e74ac89226df04c620
Randall3475 <132885679+Randall3475@users.noreply.github.com>
2023-05-17 18:59:02 +0200

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
     <>
       <section className="bg-cover bg-bottom md:bg-[url('../public/images/desktop/image-header.jpg')] bg-[url('../public/images/mobile/image-header.jpg')] h-screen flex flex-col justify-center items-center">
         <div className="flex flex-col items-center gap-y-24 md:mb-48 mb-24 px-4">
-          <h1 className="md:text-6xl text-4xl text-white font-body uppercase tracking-widest text-center">
+          <h1 className="md:text-[56px] text-[40px] text-white font-body uppercase tracking-widest text-center">
             We are creatives
           </h1>
           <ArrowDownIcon />
